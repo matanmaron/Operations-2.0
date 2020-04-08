a50e7073b14d2e545f0d35419730c9c9374caa1a
Matan Maron <matan.maron@pixigaming.com>
2020-04-08 14:10:58 +0300

    start gui

---
+++ Assets/Scripts/Call.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+public class Call
+{
+    public double CallID;
+    public string Representative;
+    public string Contents;
+    private int CallDateint;
+    public DateTime CallDate;
+    public string Remarks;
+    private int CallEndint;
+    public DateTime CallEnd;
+}
---
+++ Assets/Scripts/Reshuma.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Reshuma
+{
+    public string Company;
+    public string Zone;
+    public string PhoneNumber;
+    public DateTime EndDate;
+    public int ReshumaId;
+    public Dictionary<int, List<Call>> Calls;
+
+}
